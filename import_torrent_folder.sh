e9b55d0b1c4770a9c887f6d434d8454631eac4b6
Charles Okafor <ckokafor@hotmail.com>
2024-08-26 08:51:36 +0100

    fix bug with paths with spaces not importing

--- import_torrent_folder.sh
+++ import_torrent_folder.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Target clients
-SONARR="sonar"
-RADARR="radarr"
-
 # Operations
 NO_CONFIRM="no-confirm"
 DRY_RUN="dry-run"
-
 
 
 # TODO: Update list of directories or leave empty
@@ -23,55 +18,76 @@ SONARR_DIRECTORIES=(
 # TODO: Update operation mode
 OPERATION_MODE=$DRY_RUN
 
+###################################################################################################
+###################################################################################################
+#                                                                                                 #
+#                                          START OF EXECUTION                                     #
+#                                                                                                 #
+###################################################################################################
+###################################################################################################
 
-
-# Execute command
 # TODO: Update path to script (if required)
 cd $HOME/docker-apps/plex-media-server/blackhole-scripts
 
 ##################################################
-# RADARR                                         #
+# RADARR EXECUTION                               #
 ##################################################
 
-if [ "$OPERATION_MODE" -eq "dry-run" ]: then
+if [ "$OPERATION_MODE" == "dry-run" ]; then
     echo "RADARR: Dry Run Imports Started"
 else
     echo "RADARR: Imports Started"
 fi
 
+echo "_____________________________________________"
 echo
-echo "__________________________________________________________________________________"
 
 # Radar importing
-for DIRECTORY in $RADARR_DIRECTORIES
+for RADARR_DIRECTORY in "${RADARR_DIRECTORIES[@]}"
 do
-    if [ "$DIRECTORY" != "" ]; then
-        python3 import_torrent_folder.py --directory $DIRECTORY --$RADARR --$OPERATION_MODE
+    if [ "$RADARR_DIRECTORY" != "" ]; then
+        python3 import_torrent_folder.py --directory "$RADARR_DIRECTORY" --radarr --$OPERATION_MODE
     fi
 done
 
 echo
+
+if [ "$OPERATION_MODE" == "dry-run" ]; then
+    echo "RADARR: Dry Run Imports Completed"
+else
+    echo "RADARR: Imports Completed"
+fi
+
+echo
 echo
 
 ##################################################
-# SONARR                                         #
+# SONARR EXECUTION                               #
 ##################################################
 
-if [ "$OPERATION_MODE" -eq "dry-run" ]: then
+if [ "$OPERATION_MODE" == "dry-run" ]; then
     echo "SONARR: Dry Run Imports Started"
 else
     echo "SONARR: Imports Started"
 fi
 
+echo "_____________________________________________"
 echo
-echo "__________________________________________________________________________________"
 
 # Sonarr importing
-for DIRECTORY in $SONARR_DIRECTORIES
+for SONARR_DIRECTORY in "${SONARR_DIRECTORIES[@]}"
 do
-    if [ "$DIRECTORY" != "" ]; then
-        python3 import_torrent_folder.py --directory $DIRECTORY --$SONARR --$OPERATION_MODE
+    if [ "$SONARR_DIRECTORY" != "" ]; then
+        python3 import_torrent_folder.py --directory "$SONARR_DIRECTORY" --sonar --$OPERATION_MODE
     fi
 done
+
+echo
+
+if [ "$OPERATION_MODE" == "dry-run" ]; then
+    echo "SONARR: Dry Run Imports Completed"
+else
+    echo "SONARR: Imports Completed"
+fi
 
 echo
