242d76d02615e98192ca425c1926ca6b748fdf55
Charles Okafor <ckokafor@hotmail.com>
2024-09-25 08:51:56 +0100

    update healthcheck path

---
+++ healthcheck/watchlist/healthcheck.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TARGET=192.168.0.62
+URLS=(
+"https://www.google.com"
+"https://www.pinging.net"
+)
+# Pick a random url from the list
+URL=${URLS[ $RANDOM % ${#URLS[@]} ]}
+
+internal_status_code=$(curl --connect-timeout 10 -o /dev/null -s -w "%{http_code}" "http://${TARGET}:32400/identity")
+external_status_code=$(curl --connect-timeout 10 -o /dev/null -s -w "%{http_code}" "$URL")
+
+echo "External URL: $URL | external status code: $external_status_code | internal status code: $internal_status_code"
+
+if [ "$external_status_code" -eq 200 ] && [ "$internal_status_code" -eq 200 ]; then
+    exit 0
+else
+    exit 1
+fi
+
